9aba37448cd1b799d69e23ab1c160bf0251ecec4
AleksyBalazinski <aleksy.j.balazinski@gmail.com>
2023-03-07 14:43:00 +0100

    add crossover operators

---
+++ TravelingSalesman/OrdinalRepresentaionConverter.cs
@@ -0,0 +1,33 @@
+﻿namespace TravelingSalesman
+{
+    public class OrdinalRepresentaionConverter
+    {
+        public static int[] ToOrdinalRepresentation(int[] currentTour, int[] canonicTour)
+        {
+            List<int> ordinalRepresentation = new();
+            List<int> canonic = new(canonicTour);
+            foreach (int c in currentTour)
+            {
+                ordinalRepresentation.Add(canonic.IndexOf(c));
+                canonic.Remove(c);
+            }
+
+            return ordinalRepresentation.ToArray();
+        }
+
+        public static int[] FromOrdinalRepresentaion(int[] ordinalRepresentaation, int[] canonicTour)
+        {
+            List<int> tour = new();
+            List<int> canonic = new(canonicTour);
+
+            foreach (int i in ordinalRepresentaation)
+            {
+                int c = canonic[i];
+                tour.Add(c);
+                canonic.Remove(c);
+            }
+
+            return tour.ToArray();
+        }
+    }
+}
